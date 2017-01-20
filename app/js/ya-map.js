a03e66b984e3df4dee7a6516410671df87f12c9a
Илья Калинин <kalinination@gmail.com>
2017-01-20 10:32:43 +0300

    pics moved to /mobile/

--- app/js/ya-map.js
+++ app/js/ya-map.js
@@ -1,13 +1,12 @@
 // Создает обработчик события window.onLoad
 YMaps.jQuery(function() {
   // Создает экземпляр карты и привязывает его к созданному контейнеру
-
   var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
   map.addControl(new YMaps.Zoom());
   // Создает стиль значка метки
   var s = new YMaps.Style();
   s.iconStyle = new YMaps.IconStyle();
-  s.iconStyle.href = "/images/map-pin.png";
+  s.iconStyle.href = "/images/mobile/map-pin.png";
   s.iconStyle.size = new YMaps.Point(67, 100);
   s.iconStyle.offset = new YMaps.Point(-33, -100);
   var placemark = new YMaps.Placemark(new YMaps.GeoPoint(30.329370,59.934989), {
