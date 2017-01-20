af3cb59a03f58209a1d3d69d0531d9c26a02f39d
Илья Калинин <kalinination@gmail.com>
2017-01-20 18:04:16 +0300

    slider now working

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,18 @@
 console.log('This is the Main.js file. It should be the 3rd and final file');
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slider-container");
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length };
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
